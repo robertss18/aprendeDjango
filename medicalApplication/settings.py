d99ec34f77a7c94f77f58368285aeffaccb18915
Roberts Aguilar Portatil <roberts200018@gmail.com>
2024-03-26 10:00:26 +0100

    Actualización 26/03/24

--- medicalApplication/settings.py
+++ medicalApplication/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fw%q_lj(g64(#43ihw*%g3!wr*y9e!r%rtwrm#lrc(lsa(l7fs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- members/models.py
+++ members/models.py
@@ -5,3 +5,5 @@ class miModeloTable(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     email = models.EmailField()
+    phone = models.IntegerField(null=True)
+    joined_date = models.DateField(null=True)
